d0b32361ad25692943ca371041d52ab3b12c6495
Jhonathan Araujo <jhonathanaraujobr@hotmail.com>
2020-10-22 14:17:51 -0300

    Adicionar arquivos de projeto.

---
+++ Areas_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Areas_MVC.Startup))]
+namespace Areas_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Testes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Web;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in Power(2, 10))
+               Console.Write("{0} ", i);
+
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<int> Power(int number, int exponent)
+        {
+            int counter = 0;
+            int result = 1;
+            while (counter++ < exponent)
+            {
+                result = result * number;
+
+                if (result <= 256)
+                    yield return result;
+                else
+                    yield break;
+            }
+        }
+    }
+}
